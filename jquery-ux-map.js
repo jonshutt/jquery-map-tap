ad961fc0f9490cc89013e94107c6a686f98784e3
Jon Shutt <hello@jonshutt.com>
2015-12-13 10:35:40 +0000

    update plugin

--- jquery-ux-map.js
+++ jquery-ux-map.js
@@ -1,4 +1,3 @@
-
 // jquery closure
 (function ( $ ) {
 	//alert('x')
@@ -7,62 +6,93 @@
 	$.fn.mapper = function(options) {
 		
 		var defaults = {
-			
+			lat: 57,
+			lng: -2,
+			zoom: 5,
+			type: 'ROADMAP'
 		};
 		
-		var settings = $.extend( {}, defaults, options );
-		
+		var settings = $.extend( {}, defaults, options );		
 
+		var maparea = $(this);
+
+
+		maparea.html('<div class="mapwrap"><div class="map"></div><div class="overlay"></div><div class="btn btn-default btn-expand">Full Screen <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span></div><div class="btn btn-default btn-close">Close <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div></div>');
+
+		var mapwrap = 	maparea.find('.mapwrap') ;
+		var overlay = maparea.find('.overlay') ;
+		var map = maparea.find('.map');
+
+		maparea.on('mousedown', '.overlay', {mapwrap:mapwrap}, function(e){
+
+
+			var mapOffset = maparea.offset().top;
+			var mapHeight = maparea.height();
+			var windowHeight = $(window).height();
+
+			if (mapHeight > (windowHeight  * .75 )) {
+				maparea.height(windowHeight  * .75)
+				mapHeight = (windowHeight  * .75);
+			}
+
+			var offset;
+			offset = mapOffset - ((windowHeight / 2) - (mapHeight / 2));
+			var speed = 300;
+			$('html body').animate({scrollTop:offset}, speed, function(){
+
+			mapwrap.addClass('active');
+			
+			setTimeout(function(){
+
+				$(window).on('scroll', {mapwrap:mapwrap}, function(e){
+					e.data.mapwrap.removeClass('active');
+					$(window).off('scroll');
+					console.log('scroll, removing active');
+				});
+
+
+				$(mapwrap).on('touchstart', function(e){ 
+					e.stopPropagation();
+				});
+
+				$(window).on('touchstart', {mapwrap:mapwrap}, function(e){
+					e.data.mapwrap.removeClass('active');
+					$(window).off('touchstart');
+					console.log('touch, removing active');
+				});
+
+
+	  		}, 100);
+
+		});
+
+
+	});
+
+
+	maparea.on('click', '.btn-expand', {mapwrap:mapwrap}, function(e){
+		e.preventDefault();
+		e.data.mapwrap.addClass('expanded active');
+		google.maps.event.trigger(map, "resize");
+
+	})
+
+	maparea.on('click', '.btn-close', {mapwrap:mapwrap}, function(e){
+		e.preventDefault();
+		e.data.mapwrap.removeClass('expanded active');
+		google.maps.event.trigger(map, "resize");
+
+	})
+
+	var mapProp = {
+	    center:new google.maps.LatLng(settings.lat,settings.lng),
+	    zoom:settings.zoom,
+	    mapTypeId:google.maps.MapTypeId[settings.type],
+	    draggable: true,
+			scrollwheel: true
+	};
 	
-
-		var map = $(this);
-
-		//map.addClass('xxx');
-		//map.attr('id', 'map1');
-
-		map.wrap('<div class="mapwrap">');
-
-		var wrap = map.closest('.mapwrap');
-
-		map.after('<div class="overlay">');
-
-
-		wrap.on('mousedown', '.overlay', {wrap:wrap}, function(e){
-			e.data.wrap.addClass('active');
-			//console.log(e);
-		});
-
-		wrap.on('mouseout', {wrap:wrap}, function(e){
-			//e.data.wrap.removeClass('active');
-			//console.log(e);
-		});
-
-
-		/*wrap.on('mousewheel', {wrap:wrap}, function(e){
-			e.data.wrap.addClass('active');
-			//console.log(e);
-		});
-*/
-
-
-
-		$(window).on('scroll', {wrap:wrap}, function(e){
-			//e.data.wrap.removeClass('active');
-			//console.log(e);
-		});
-
-
-		var mapProp = {
-		    center:new google.maps.LatLng(51.508742,-0.120850),
-		    zoom:5,
-		    mapTypeId:google.maps.MapTypeId.ROADMAP,
-		    draggable: true,
-  			scrollwheel: true
-		  };
-
-		 // console.log(map[0])
-		 // console.log(document.getElementById("map1"));
-		  var map=new google.maps.Map(map[0],mapProp);
+	var map=new google.maps.Map(map[0],mapProp);
 
 		
 	};
